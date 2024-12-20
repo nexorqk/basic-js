5f67d34ec81b468e299a6d4bd9ad328676e0c057
nexorqk <pkms2a10@gmail.com>
2024-12-20 17:15:51 +0300

    feat: add common character count solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -17,6 +17,34 @@ function getCommonCharacterCount(s1, s2) {
 
   s1.split("").forEach((item) => (count1[item] = (count1[item] || 0) + 1));
   s2.split("").forEach((item) => (count2[item] = (count2[item] || 0) + 1));
+
+  const commonObj = {};
+
+  if (Object.keys(count1).length > Object.keys(count2).length) {
+    Object.keys(count1).forEach((item) => {
+      if (count2[item] !== undefined) {
+        count1[item] > count2[item]
+          ? (commonObj[item] = count2[item])
+          : (commonObj[item] = count1[item]);
+      }
+    });
+  } else {
+    Object.keys(count2).forEach((item) => {
+      if (count1[item] !== undefined) {
+        count1[item] > count2[item]
+          ? (commonObj[item] = count2[item])
+          : (commonObj[item] = count1[item]);
+      }
+    });
+  }
+
+  let sum = 0;
+
+  for (let key in commonObj) {
+    sum += commonObj[key];
+  }
+
+  return sum;
 }
 
 module.exports = {
