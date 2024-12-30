055649392fbfd880f5582cc2eec09ccb5c448f9f
nexorqk <pkms2a10@gmail.com>
2024-12-30 17:09:38 +0300

    feat: add extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, option) {
+  let res = "";
+
+  for (let i = 0; i < option.repeatTimes; i++) {
+    if (i > 0 && option.separator === undefined) {
+      res += "+" + str;
+    } else {
+      res += str;
+    }
+
+    if (option.additionRepeatTimes !== undefined) {
+      for (let j = 0; j < option.additionRepeatTimes; j++) {
+        if (j > 0 && !option.additionSeparator) {
+          res += "|";
+        }
+
+        res += option.addition;
+
+        if (option.additionSeparator && j !== option.additionRepeatTimes - 1) {
+          res += option.additionSeparator;
+        }
+      }
+    }
+
+    if (!option.additionRepeatTimes && option.addition !== undefined) {
+      res += option.addition;
+    }
+
+    if (option.separator && i !== option.repeatTimes - 1) {
+      res += option.separator;
+    }
+  }
+
+  if (option.repeatTimes === undefined) {
+    res += str;
+    res += option.addition;
+  }
+
+  return res;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
